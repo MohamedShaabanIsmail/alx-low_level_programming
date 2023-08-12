16544d3315d84edca5f93a66c034d5edb21d0005
MohamedShaabanIsmail <mohamed_shaaban_is@outlook>
2023-08-11 18:52:49 -0700

    file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - start main
+ *
+ * Description: write separated digit
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		putchar(i);
+		if (i != 9)
+			putchar(', ');
+	}
+	putchar('\n');
+	return (0);
+}
