e66d1903f8bc85a3046b0ca1cd70f14d4d0912b4
MohamedShaabanIsmail <mohamed_shaaban_is@outlook>
2023-08-11 18:59:03 -0700

    file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,12 @@
 */
 int main(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	for(i = 0; i < 10; i++)
 	{
 		putchar(i);
 		if (i != 9)
-			putchar(', ');
+			putchar(", ");
 	}
 	putchar('\n');
 	return (0);
