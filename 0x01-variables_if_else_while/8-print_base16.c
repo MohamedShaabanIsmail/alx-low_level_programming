a9184d2baba8d38222b78ed84d786c0264970f4e
MohamedShaabanIsmail <mohamed_shaaban_is@outlook>
2023-08-14 19:47:28 -0700

    file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+/**
+ * main - start main
+ * Description: write hexa number
+ * Return: 0 (success)
+*/
 int main(void)
 {
 	int i, y;
 
 	for (i = 0; i < 10 ; i++)
 		putchar('0' + i);
-	for (y = 0; y < 7 ; y++)
-		putchar('0' + y);
+	for (y = 'a'; y <= 'f' ; y++)
+		putchar(y);
 	putchar('\n');
 	return (0);
 }
