ce21d3731f4c7b6864fb5db3a65d4cd0e74afad6
MohamedShaabanIsmail <mohamed_shaaban_is@outlook.com>
2023-08-17 17:21:16 -0700

    cfile

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 /**
- * _isupper - check if x is upper case
- * @x: parameter for function
- * Return: 1 or 0
- */
-int _isupper(int x)
-{
-	if (x >= 65 && x <= 90)
-		return (1);
-	return (0);
-}
-/**
  * main - start main
  * Description: check function
  * Return: Always 0.
