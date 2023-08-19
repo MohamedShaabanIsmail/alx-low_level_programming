cdc2cbcaa2d9ea60536fda42c2b38112574afcb3
MohamedShaabanIsmail <mohamed_shaaban_is@outlook.com>
2023-08-18 18:15:52 -0700

    filec

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0 && i != 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
-		else if (i % 3 == 0 && i != 0)
+		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if (i % 5 == 0 && i != 0)
+		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
