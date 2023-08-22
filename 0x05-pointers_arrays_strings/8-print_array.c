0998bc0ed7e8f134d512b4303a4424e5ed95c85a
MohamedShaabanIsmail <mohamed_shaaban_is@outlook.com>
2023-08-21 18:41:19 -0700

    file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_array - function
+ * @a: first parameter
+ * @n: second parameter
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d",a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_strcpy - function
+ * @dest: parameter1
+ * @src: parameter2
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
